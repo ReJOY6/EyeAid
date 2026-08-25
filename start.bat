5d84f6fd27f9f47960515acb7784ec671b02abc4
adnan <nur0adnan@gmail.com>
2026-08-25 19:25:51 +0200

    bat-file | edit

--- start.bat
+++ start.bat
@@ -1,5 +1,16 @@
-@echo off
+    @echo off
+cd /d "%~dp0"
+echo ==========================================
 echo Starte EyeAid... Bitte warten...
+echo ==========================================
+
+:: 1. Sicherstellen, dass die Bibliotheken installiert sind
 pip install -r requirements.txt
+
+:: 2. Das Programm starten
 python app.py
+
+:: 3. Verhindert, dass das Fenster bei einem Fehler einfach abstürzt
+echo.
+echo Das Programm wurde beendet oder es gab einen Fehler.
 pause
